c0304eddd5c1288a7814dfada8f1a3ff247e54d5
abiyyu1564 <abiyyuabdurrafi14@gmail.com>
2024-08-30 17:16:47 +0700

    feat: Tugas hari ke-10: next image & dynamic routing

--- nextjs-learning/pages/index.js
+++ nextjs-learning/pages/index.js
@@ -1,17 +1,22 @@
-import Layout from "@/layout";
 import { useEffect } from "react";
+import Image from "next/image";
+import dynamic from "next/dynamic";
+
+const LayoutComponent = dynamic(() => import("@/layout"));
 
 export default function Home({ children }) {
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((res) => console.log("response =>", res));
-    // .catch((err) => console.log(err));
-  }, []);
+  // useEffect(() => {
+  //   fetch("/api/hello")
+  //     .then((res) => res.json())
+  //     .then((res) => console.log("response =>", res));
+  //   // .catch((err) => console.log(err));
+  // }, []);
 
   return (
-    <Layout metaTitle={"Home"}>
+    <LayoutComponent metaTitle={"Home"}>
       <h1>Home</h1>
-    </Layout>
+      {/* <Image src="/police.jpg" width={200} height={200} alt="Police" />
+      <img src="/police.jpg" style={{ width: 200, height: 200 }} alt="Police" /> */}
+    </LayoutComponent>
   );
 }
